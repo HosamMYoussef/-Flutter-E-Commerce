e646338c541b32435cdd539f0817ca0f2138c659
Hosam Youssef <hosamyoussef303@gmail.com>
2022-09-19 12:04:09 +0200

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myshopp/constants.dart';
 import 'package:myshopp/core/viewmodel/checkoutview_model.dart';
 import 'package:myshopp/core/viewmodel/home_view_model.dart';
 import 'package:myshopp/view/auth/login_screen.dart';
@@ -6,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:myshopp/view/control_view.dart';
+import 'package:page_transition/page_transition.dart';
 import 'core/viewmodel/cart_view_model.dart';
 import 'firebase_options.dart';
 import 'helper/binding.dart';
@@ -19,7 +24,6 @@ Future<void> main() async {
   );
   Get.put(CartViewModel());
   //Get.put(CheckoutViewModel());
-
 
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
@@ -40,9 +44,20 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       initialBinding: Binding(),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: ControlView(),
-        //bottomNavigationBar: curvedNavigationBar(),
+      home: AnimatedSplashScreen(
+        duration: 2000,
+        splashTransition: SplashTransition.fadeTransition,
+        splashIconSize: 400,
+        
+        splash:SvgPicture.asset(
+                    'assets/images/welcomeCat.svg',
+                    // width: 350,
+                    // height: 800,
+                  ),
+        nextScreen: ControlView(),
+        //backgroundColor: primaryColor,
+        pageTransitionType: PageTransitionType.topToBottom as dynamic,
+        backgroundColor: Colors.grey.shade100,
       ),
       theme: ThemeData(
         fontFamily: 'OpenSans',
@@ -50,3 +65,15 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+// enum PageTransitionType {
+//   fade,
+//   rightToLeft,
+//   leftToRight,
+//   upToDown,
+//   downToUp,
+//   scale,
+//   rotate,
+//   size,
+//   rightToLeftWithFade,
+//   leftToRightWithFade,
+// }
